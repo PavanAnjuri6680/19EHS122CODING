247a73c8af48f8b8bf7931a0c3db00ad3dc2311f
PavanAnjuri6680 <83009059+PavanAnjuri6680@users.noreply.github.com>
2021-07-01 18:45:53 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int num_1=10, num_2, sum;
+  num_2 = 20;
+  sum=num_1 +num_2;
+  num_1 = 25;
+  num_2 = 50;
+ sum = num_1 + num_2;
+printf("Num 1 =%d\nNum 2 = %d\n", num_1, num_2);
+printf("Sum = %d\n\n\n", sum);
+
+  return 0;
+}
